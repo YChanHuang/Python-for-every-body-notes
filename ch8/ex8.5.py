59d262b45fe1fe94b49a16693ba1bee389187127
YChanHuang <j3505151@gmail.com>
2020-09-24 11:05:36 +0100

    Week4 finished

--- ch8/ex8.5.py
+++ ch8/ex8.5.py
@@ -1,13 +1,17 @@
 #Assignment 8.5
+#file name = mbox-short.txt
 fname = input("Enter file name: ")
 if len(fname) < 1 : fname = "mbox-short.txt"
 
 fh = open(fname)
 count = 0
-lst = []
+
 for line in fh:
     line = line.rstrip()
+    if not line.startswith('From '): #To check if the line staty with 'From '
+            continue    #Note that there is a space behind the From, otherwise the print resuly would duplicated
     word = line.split()
-    lst.append(word)
-print(lst)
-#print("There were", count, "lines in the file with From as the first word")
+    count = count + 1
+    print(word[1]) #
+
+print("There were", count, "lines in the file with From as the first word")
